4c931c67f633a93f7f5dd35352eb8a8a6ac51a97
Chu Ngọc Thắng <chuthanglsz@gmail.com>
2026-04-30 11:15:26 +0700

    fix: build gateway

--- api-gateway/src/main/java/com/backend/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/backend/gateway/ApiGatewayApplication.java
@@ -2,12 +2,29 @@ package com.backend.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public GlobalFilter customGlobalFilter() {
+		return (exchange, chain) -> {
+			log.info("GATEWAY RECEIVING REQUEST: {} {}", 
+                exchange.getRequest().getMethod(), 
+                exchange.getRequest().getURI());
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				log.info("GATEWAY RESPONDING WITH STATUS: {}", 
+                    exchange.getResponse().getStatusCode());
+			}));
+		};
+	}
 }
