b5126f449e782a4d52dcb02cd6e371b9a28cfb88
Chu Ngọc Thắng <chuthanglsz@gmail.com>
2026-04-30 12:52:47 +0700

    fix: build gateway

--- api-gateway/src/main/java/com/backend/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/backend/gateway/ApiGatewayApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @Slf4j
@@ -27,4 +32,18 @@ public class ApiGatewayApplication {
 			}));
 		};
 	}
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
 }
